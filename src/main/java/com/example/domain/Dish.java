776484b0b38ebf995b0d881e1448c078145df173
Hector Bautista <hbautistaj@gmail.com>
2015-11-16 02:05:05 -0600

    Adding equals & hashcode methods for Dish & Category classes

--- src/main/java/com/example/domain/Dish.java
+++ src/main/java/com/example/domain/Dish.java
@@ -1,8 +1,7 @@
 package com.example.domain;
 
-import java.util.HashSet;
 import java.util.Set;
-
+import java.util.HashSet;
 import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
@@ -15,58 +14,78 @@ public class Dish {
 	private String name;
 	private String ingredients;
 	@Relationship(type = "IS", direction = Relationship.OUTGOING)
-	private Set<Category> categories;
+    private Set<Category> categories;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public int getDishID() {
-		return dishID;
-	}
-
-	public void setDishID(int dishID) {
-		this.dishID = dishID;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getIngredients() {
-		return ingredients;
-	}
-
-	public void setIngredients(String ingredients) {
-		this.ingredients = ingredients;
-	}
-	
-	public void hasCategory(Category category) {
-        if (categories == null) {
-            categories = new HashSet<Category>();
-        }
-        categories.add(category);
+    public Long getId() {
+        return id;
     }
 
-	public Set<Category> getCategories() {
-		return categories;
-	}
+    public int getDishID() {
+        return dishID;
+    }
 
-	public void setCategories(Set<Category> categories) {
-		this.categories = categories;
-	}
+    public void setDishID(int dishID) {
+        this.dishID = dishID;
+    }
 
-	@Override
-	public String toString() {
-		return "Dish [id=" + id + ", dishID=" + dishID + ", name=" + name + ", ingredients=" + ingredients + "]";
-	}
-	
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+                return true;
+        if (obj == null)
+                return false;
+        if (getClass() != obj.getClass())
+                return false;
+        Dish other = (Dish) obj;
+        if (name == null) {
+                if (other.name != null)
+                        return false;
+        } else if (!name.equals(other.name))
+                return false;
+        return true;
+    }
+
+    public void addCategories(Category category) {
+        if(categories == null)
+            categories = new HashSet<Category>();
+        else if(!categories.contains(category))
+            categories.add(category);
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + " name: " + name + "ingredients: " + ingredients + " dishID: " + dishID;
+    }
 }
