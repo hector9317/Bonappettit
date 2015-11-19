aecb82a707af48194cd22bb852e1a78b5266eba0
Azucena López <blankazucenalg@gmail.com>
2015-11-18 19:49:50 -0600

    DAO sources

---
+++ src/main/java/com/aht/dao/category/CategoryDAO.java
@@ -0,0 +1,15 @@
+package com.aht.dao.category;
+
+import java.util.LinkedList;
+
+import com.aht.domain.Category;
+
+public interface CategoryDAO {
+	public void create(Category category);
+	public Category retrieve(long id);
+	public void update(Category category);
+	public void delete(Category category);
+	public LinkedList<Category> retrieveAll();
+	public Category findByName(String name);
+	public boolean exists(Category category);	
+}
---
+++ src/main/java/com/aht/dao/dish/DishDAO.java
@@ -0,0 +1,15 @@
+package com.aht.dao.dish;
+
+import java.util.LinkedList;
+
+import com.aht.domain.Dish;
+
+public interface DishDAO {
+	public void create(Dish dish);
+	public Dish retrieve(long dishID);
+	public void update(Dish dish);
+	public void delete(Dish dish);
+	public LinkedList<Dish> retrieveAll();
+	public Dish findByName(String name);
+	public boolean exists(Dish dish);
+}
